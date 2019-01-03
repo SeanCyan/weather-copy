5f776859d270820386507cf9258d0887abd21a01
Sean Nattress <snattress5@googlemail.com>
2019-01-03 00:10:50 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import FrontSide from './FrontSide.js';
+import moment from 'moment';
+import './panel.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="panel">
+        <FrontSide 
+          date={moment()} 
+          icon='default' 
+          temperature={19} 
+          apparentTemperature={15} 
+          summary='Sunny'
+          currentCityName='Stockholm' 
+          />
       </div>
     );
   }
