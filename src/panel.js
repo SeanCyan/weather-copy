9d3bdb2930929207c1c6102d2a0dc8375c04f6bd
Sean Nattress <snattress5@googlemail.com>
2019-01-13 22:50:52 +0000

    minor update

--- src/panel.js
+++ src/panel.js
@@ -8,7 +8,7 @@ class Panel extends React.Component {
             <div className="panel">
                 <h2 className="caption">HOWS THE WEATHER?</h2>
                 <div className={`card a${this.props.weatherDayOne.icon}`}>
-                    <img src={`/icons/${this.props.weatherDayOne.icon}.png`} alt={`${this.props.weatherDayOne.description}`} className='card-icon'/>
+                    <img src={`icons/${this.props.weatherDayOne.icon}.png`} alt={`${this.props.weatherDayOne.description}`} className='card-icon'/>
                     <h3>{`${this.props.weatherDayOne.description}`}</h3>
                     <p>{`${this.props.dateDayOne}`}</p>
                     <div className='card-row'>
@@ -21,7 +21,7 @@ class Panel extends React.Component {
                     <div className='card-city'>{this.props.city.name}, {this.props.city.country}</div>
                 </div>
                 <div className={`card a${this.props.weatherDayTwo.icon}`}>
-                    <img src={`/icons/${this.props.weatherDayTwo.icon}.png`} alt={`${this.props.weatherDayTwo.description}`} className='card-icon'/>
+                    <img src={`icons/${this.props.weatherDayTwo.icon}.png`} alt={`${this.props.weatherDayTwo.description}`} className='card-icon'/>
                     <h3>{`${this.props.weatherDayTwo.description}`}</h3>
                     <p>{`${this.props.dateDayTwo}`}</p>
                     <div className='card-row'>
@@ -34,7 +34,7 @@ class Panel extends React.Component {
                     <div className='card-city'>{this.props.city.name}, {this.props.city.country}</div>
                 </div>
                 <div className={`card a${this.props.weatherDayThree.icon}`}>
-                    <img src={`/icons/${this.props.weatherDayThree.icon}.png`} alt={`${this.props.weatherDayThree.description}`} className='card-icon'/>
+                    <img src={`icons/${this.props.weatherDayThree.icon}.png`} alt={`${this.props.weatherDayThree.description}`} className='card-icon'/>
                     <h3>{`${this.props.weatherDayThree.description}`}</h3>
                     <p>{`${this.props.dateDayThree}`}</p>
                     <div className='card-row'>
@@ -47,7 +47,7 @@ class Panel extends React.Component {
                     <div className='card-city'>{this.props.city.name}, {this.props.city.country}</div>
                 </div>
                 <div className={`card a${this.props.weatherDayFour.icon}`}>
-                    <img src={`/icons/${this.props.weatherDayFour.icon}.png`} alt={`${this.props.weatherDayFour.description}`} className='card-icon'/>
+                    <img src={`icons/${this.props.weatherDayFour.icon}.png`} alt={`${this.props.weatherDayFour.description}`} className='card-icon'/>
                     <h3>{`${this.props.weatherDayFour.description}`}</h3>
                     <p>{`${this.props.dateDayFour}`}</p>
                     <div className='card-row'>
@@ -60,7 +60,7 @@ class Panel extends React.Component {
                     <div className='card-city'>{this.props.city.name}, {this.props.city.country}</div>
                 </div>
                 <div className={`card a${this.props.weatherDayFive.icon}`}>
-                    <img src={`/icons/${this.props.weatherDayFive.icon}.png`} alt={`${this.props.weatherDayFive.description}`} className='card-icon'/>
+                    <img src={`icons/${this.props.weatherDayFive.icon}.png`} alt={`${this.props.weatherDayFive.description}`} className='card-icon'/>
                     <h3>{`${this.props.weatherDayFive.description}`}</h3>
                     <p>{`${this.props.dateDayFive}`}</p>
                     <div className='card-row'>
